0b4547761be61d22ba683a8acddbddc5996ab48a
Yurui-Feng <yurifeng@bu.edu>
2024-03-01 01:37:17 -0800

    add http routes for likes and checks

--- backendFlask/core.py
+++ backendFlask/core.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, jsonify, abort
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from dataclasses import dataclass
 from sqlalchemy import UniqueConstraint
+import requests
 
 # Create a new Flask application
 core = Flask(__name__)
@@ -39,6 +40,47 @@ class HouseChecker(db.Model):
 @core.route('/')
 def index():
     return 'Hello, World!'
+
+@core.route('/api/houses')
+def index():
+    return jsonify(House.query.all())
+
+@core.route('/api/houses/<int:id>/like', methods=['POST'])
+def like(id):
+    req = requests.get('http://localhost:5002/api/checker')
+    json = req.json()
+
+    try:
+        # Create a new houseChecker with the checker id and the house id
+        houseChecker = HouseChecker(checker_id=json['id'], house_id=id)
+        # Save the new houseChecker
+        db.session.add(houseChecker)
+        db.session.commit()
+    except:
+        abort(400, 'You already liked this house')
+    
+    return jsonify({
+        'message': 'success'
+    })
+
+@core.route('/api/houses/<int:id>/check', methods=['POST'])
+def check(id):
+    req = requests.get('http://localhost:5002/api/checker')
+    json = req.json()
+
+    try:
+        # Create a new houseChecker with the checker id and the house id
+        houseChecker = HouseChecker(checker_id=json['id'], house_id=id)
+        # Save the new houseChecker
+        db.session.add(houseChecker)
+        db.session.commit()
+    except:
+        abort(400, 'You already checked this house')
+    
+    return jsonify({
+        'message': 'success'
+    })
+    
 
 if __name__ == '__main__':
     core.run(debug=True, host='0.0.0.0', port=5001)
